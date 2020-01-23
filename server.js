1425d829a02307836f97cdcfece923e13e3ca850
Zachary Horine <horinez@oregonstate.edu>
2020-01-23 14:59:57 -0800

    Add recipe info and test data

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/', function (req, res, next) {
 });
 
 app.get('/recipe', function (req, res, next) {
-   var recipe = {title: "Test", author: "John Doe", date: "11/01/2012"};
+   var recipe = {title: "Test", author: "John Doe", date: "11/01/2012", preptime: "15 minutes", cooktime: "20 minutes", servings: 8, source: "allrecipes.com", sourceLink: "https://www.allrecipes.com/recipe/214614/chicken-parmesan-pasta-casserole/"};
    var ingredients = {1:{size: 1, measurement: "cup", name: "sugar"}};
    var instructions = {1:{step: 1, text: "Add one cup of sugar to the bowl."}};
    res.status(200).render('recipe', {
