ba4aed116c2e4ce210bdb394efe0e90a6c9a720b
Zachary Horine <horinez@oregonstate.edu>
2020-02-04 02:45:36 -0800

    update output format and add title

--- jsonimport.js
+++ jsonimport.js
@@ -3,17 +3,37 @@ var ingredient = require('./parseIngredients.js');
 var jsonIn = require('./jsonOut.json');
 
 
+console.log(Object.values(jsonIn).length);
+
+for (q = 0; q < jsonIn.length; q++) {
+   console.log("---"+q+"---");
+   console.log(getRecipeData(jsonIn[q]));
+   console.log(getIngredients(jsonIn[q]));
+   console.log(getInstructions(jsonIn[q]));
+}
+
+function getRecipeData(recipe) {
+   var ret = [];
+   ret.push(recipe.title);
+   ret.push(recipe.time);
+   return ret;
+}
 function getIngredients(recipe) {
+   var ret = [];
    var ingredients = Object.values(recipe.ingredients);
    for (r = 0; r < ingredients.length; r++) {
-      console.log(ingredients[r]);
-      console.log(ingredient.parse(ingredients[r]));
+      ret.push(ingredient.parse(ingredients[r]));
    }
+   return ret;
 }
 
 function getInstructions(recipe) {
+   var ret = [];
    var res = recipe.directions.split("\n");
    for (r = 0; r < res.length; r++) {
-      console.log(r+1+". "+res[r]);
+      var itemNum = r+1;
+      var dir = res[r];
+      ret.push({itemNum,dir});
    }
+   return ret;
 }
