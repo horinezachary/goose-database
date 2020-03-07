7643aa4be2fd4a9b7e9d7b4469889ca889764f38
RyanCryar <ryan.cryar@global-formula-racing.com>
2020-03-06 19:02:02 -0800

    added stuff that parses through all of cafedelites. will be getting more data soon

--- parse.py
+++ parse.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 
-holdurl='https://www.epicurious.com'
+holdurl='https://www.cafedelites.com'
 
-url = 'https://www.epicurious.com/search/?content=recipe'
-pageadd='&page='
+url = 'https://cafedelites.com/recipes/'
+pageadd='page/'
 f = open('recipes.txt', 'a')
 i = 1
 while i:
-    if i == 700:
+    if i == 90:
         break
     if i != 1:
         finalurl=url+pageadd+str(i)
@@ -26,15 +26,15 @@ while i:
         driver.execute_script("window.stop();")
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.quit()
-    job_elems = soup.find_all('', {"class":"recipe-content-card"})
-
+    job_elems = soup.find_all('', {"class":"entry-image-link"})
+   # print(job_elems)
 
     for job_elem in job_elems:
-        ref=job_elem.find('', {"class":"photo-link"})
-        final=holdurl+ref['href']
-        print(final)
-        f.write('\n')
-        f.write(final)
+        #ref=job_elem.find('', {"class":"photo-link"})
+        #final=holdurl+ref['href']
+        print(job_elem['href'])
+        #f.write('\n')
+        #f.write(final)
     
     i+=1
 f.close()
