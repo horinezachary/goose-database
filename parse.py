08c2f94a2a4b341c98a037b9401dda9ce526033e
Fischer Jemison <jemisonf@oregonstate.edu>
2020-03-10 15:59:47 -0700

    updates with parallel code

--- parse.py
+++ parse.py
@@ -4,34 +4,36 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 
-holdurl='https://www.allrecipes.com'
+holdurl = "https://www.allrecipes.com"
 
-url = 'https://www.allrecipes.com/recipes/94/soups-stews-and-chili/?internalSource=top%20hubs&referringContentType=Homepage'
-pageadd='&page='
-f = open('recipes.txt', 'a')
+url = "https://www.allrecipes.com/recipes/94/soups-stews-and-chili/?internalSource=top%20hubs&referringContentType=Homepage"
+pageadd = "&page="
+f = open("recipes.txt", "a")
 i = 2
 while i:
     if i == 500:
         break
     if i != 1:
-        finalurl=url+pageadd+str(i)
+        finalurl = url + pageadd + str(i)
     else:
-        finalurl=url
+        finalurl = url
 
-    driver = webdriver.Chrome(executable_path='/home/ryan/Documents/goose-database/chromedriver')
-    driver.set_page_load_timeout(2)
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    driver = webdriver.Chrome(options=options)
+    driver.set_page_load_timeout(5)
     try:
         driver.get(finalurl)
     except TimeoutException:
         driver.execute_script("window.stop();")
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    soup = BeautifulSoup(driver.page_source, "html.parser")
     driver.quit()
-    job_elems = soup.find_all('', {"class":"fixed-recipe-card"})
+    job_elems = soup.find_all("", {"class": "fixed-recipe-card"})
     for job_elem in job_elems:
-        ref=job_elem.find('', {"class":"grid-card-image-container"})
-        final=ref.find('a')
-        print(final['href'])
-        f.write('\n')
-        f.write(final['href'])
-    i+=1
+        ref = job_elem.find("", {"class": "grid-card-image-container"})
+        final = ref.find("a")
+        print(final["href"])
+        f.write("\n")
+        f.write(final["href"])
+    i += 1
 f.close()
--- scraperAll.py
+++ scraperAll.py
@@ -1,81 +1,99 @@
 #!/bin/python
 import sys
 import json
-from recipe_scrapers import scrape_me 
+from recipe_scrapers import scrape_me
 import requests
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from itertools import repeat
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, Manager
 
 
-f = open('recipes.txt', 'r')
+f = open("recipes.txt", "r")
+
+
 def getAuthor(soup):
     try:
-        job_elem = soup.find_all('', {"class":"submitter__name"});
+        job_elem = soup.find_all("", {"class": "submitter__name"})
         return job_elem[0].contents[0]
     except:
-        return 'none'
-        
+        return "none"
+
+
 def getYield(soup):
     try:
-        job_elem = soup.find_all('', {"class":"yield"})
+        job_elem = soup.find_all("", {"class": "yield"})
         return job_elem[1].contents[0]
     except:
-        return 'none'
+        return "none"
+
 
 def strip(instructions):
     inst = instructions.split(".")
     inst.pop()
     return inst
 
-def scrape(num):
-    fileNum=0
-    i = 0
-    fileout = open('recipeJson' + str(fileNum) + '.json', 'a')
-    fileout.write("{\n")
-    for line in f:
-        if i > 50 :
-            fileout.write("}")
-            fileout.close()
-            fileNum+=1
-            fileout = open('recipeJson' + str(fileNum) + '.json', 'a')
-            i = 0
+
+def scrape(index, total_processes, recipes):
+    # splitting the full recipes list into chunks of size len(recipes) / total_processes
+    minIndex = int(index * len(recipes) / total_processes)
+    maxIndex = minIndex + int(len(recipes) / total_processes)
+    localRecipes = recipes[minIndex:maxIndex]
+
+    # if there are "extra" recipes, give them to the last process
+    if index == total_processes - 1 and len(recipes) >= maxIndex - 1:
+        localRecipes += recipes[maxIndex:]
+
+    data = []
+    for i, recipe in enumerate(localRecipes):
+        if (i % 100) == 0:
+            with open("outRecipes_" + str(index) + "_" + str(i) + ".json", "w+") as f:
+                json.dump({"data": data}, f)
+            data = []
         jsonOut = {}
-        scraper = scrape_me(line.rstrip())
-        driver = webdriver.Chrome(executable_path='/home/ryan/Documents/goose-database/chromedriver')
-        #driver = webdriver.Chrome()
-        driver.set_page_load_timeout(1)
+        scraper = scrape_me(recipe.rstrip())
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        driver = webdriver.Chrome(options=options)
+        # driver = webdriver.Chrome()
+        driver.set_page_load_timeout(10)
         try:
-            driver.get(line.rstrip())
+            driver.get(recipe.rstrip())
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+
+            jsonOut["author"] = getAuthor(soup)
+            jsonOut["title"] = scraper.title()
+            jsonOut["ingredients"] = scraper.ingredients()
+            #      jsonOut["time"] = getTime(driver, soup)
+            jsonOut["yield"] = scraper.yields()
+            jsonOut["cook_time"] = scraper.total_time()
+            jsonOut["prep_time"] = "none"
+            jsonOut["directions"] = strip(scraper.instructions())
+            jsonOut["url"] = recipe.rstrip()
+            jsonOut["source"] = "allrecipes.com"
+            driver.quit()
+            data.append(jsonOut)
         except TimeoutException:
-            driver.execute_script("window.stop();")
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-    
-        jsonOut["author"] = getAuthor(soup)
-        jsonOut["title"] = scraper.title()
-        jsonOut["ingredients"] = scraper.ingredients()
- #      jsonOut["time"] = getTime(driver, soup)
-        jsonOut["yield"] = scraper.yields()
-        jsonOut["cook_time"] = scraper.total_time()
-        jsonOut["prep_time"] = "none"
-        jsonOut["directions"] = strip(scraper.instructions())
-        jsonOut["url"] = line.rstrip()
-        jsonOut["source"] = "allrecipes.com"
-        json.dump(jsonOut, fileout, indent=4)
-        fileout.write(",")
-        driver.quit()
-        i+=1
-        print(json.dumps(jsonOut, indent=4))
+            print("time out on " + recipe)
+    with open("outRecipes_" + str(index) + "_" + str(i) + ".json", "w+") as f:
+        json.dump({"data": data}, f)
+    data = []
 
 
+if __name__ == "__main__":
 
-
-if __name__ == '__main__':
-
-    with Pool(cpu_count()-1) as p:
-        p.starmap(scrape, zip(range(1,2000)))
+    recipes = f.read().splitlines()
+    num_cpus = cpu_count()
+    with Pool(cpu_count() - 1) as p:
+        p.starmap(
+            scrape,
+            zip(
+                range(num_cpus),
+                [num_cpus for _ in range(num_cpus)],
+                [recipes for _ in range(num_cpus)],
+            ),
+        )
     p.close()
     p.join()
     f.close()
